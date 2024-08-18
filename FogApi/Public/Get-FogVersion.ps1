ec8c95ff9a711e0958749d019c2b6ed394149a3d
JJ Fullmer <ldscellist@Gmail.com>
2024-08-17 21:55:28 -0600

    Added general idea of returning version string for Add get-fogversion #10 will need to make a branch for using it to adjust the core objects dynamically

--- FogApi/Public/Get-FogVersion.ps1
+++ FogApi/Public/Get-FogVersion.ps1
@@ -1,4 +1,19 @@
 function Get-FogVersion {
+    <#
+    .SYNOPSIS
+    Gets the version of FOG
+    
+    .DESCRIPTION
+    Only works for 1.6 beta and above, 1.5.x is identified as 1.5.10 if null is returned
+    
+    .EXAMPLE
+    Get-FogVersion;
+
+    Will return the full version string
+    
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param (
         
@@ -6,7 +21,10 @@ function Get-FogVersion {
     
     
     process {
-        return Invoke-FogApi -uriPath "system/info" -Method Get;
+        $versionStr = (Invoke-FogApi -uriPath "system/info" -Method Get).version;
+        if ([string]::IsNullOrEmpty($versionStr)) {
+            return "1.5.10";
+        }
     }
     
 }
